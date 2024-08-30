274ad7c804ebba240ffd4755e8ce503deebbaf53
Abhash Tiwari <abhashhtiwary7610@gmail.com>
2024-08-30 21:12:02 +0530

    Set up API routes and main application file

---
+++ src/app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/database');
+const apiRoutes = require('./routes/api');
+const { startPriceUpdateScheduler } = require('./controllers/priceController');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+connectDB();
+
+app.use(express.json());
+app.use('/api', apiRoutes);
+
+startPriceUpdateScheduler();
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
